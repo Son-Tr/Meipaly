504174f2921aa8fb06b5bc9867f59bfefdf92e00
Son-Tr <sontran.kh1502@gmail.com>
2024-06-22 22:29:16 +0700

    service

--- js/script.js
+++ js/script.js
@@ -7,5 +7,26 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 2000,
         dots: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 4
+                }
+              },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                centerMode: true,
+                slidesToShow: 1
+              }
+            }
+          ]
     })
 })
